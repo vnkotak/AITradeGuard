e797e80402fb64275255a77db95225f1947d0569
vnkotak <57126776+vnkotak@users.noreply.github.com>
2025-09-22 01:57:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Hello from AITradeGuard")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
