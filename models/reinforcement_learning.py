fc16dbe30894cdd0d44c1ef3cb983fbe27e60484
vnkotak <57126776+vnkotak@users.noreply.github.com>
2025-09-22 20:22:23 +0530

    Update reinforcement_learning.py

--- models/reinforcement_learning.py
+++ models/reinforcement_learning.py
@@ -11,8 +11,8 @@ from datetime import datetime, timedelta
 import pickle
 import os
 
-import gym
-from gym import spaces
+import gymnasium
+from gymnasium import spaces
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
